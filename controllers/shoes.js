449c028729a6bc1e30c02f7de27bbe37aba85bba
Matmat Myriam <matmat.myriam@gmail.com>
2017-11-19 15:55:32 +0100

    last modif

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -18,7 +18,6 @@ exports.getShoes = (req, res) =>{
                  $lte: value,
              },
          };
-         console.log(query);
          Shoes.find(query,function (err,docs) {
 
              allshoes = docs;
@@ -35,13 +34,22 @@ exports.getShoesOrder = (req, res) => {
     Shop.find({"_id" : id}, function (err, docs) {
         shop = docs[0];
         var idShop = docs[0].idShop;
+        //recup seulement si pas dans le panier
+        var value = moment().subtract(10,'minutes').format();
+        //Chercher shoes en fonction de la date
+        var query = {
+            code_shop : idShop,
+            date_cart: {
+                $lte: value,
+            },
+        };
         if(order === 'orderPrice'){
-            Shoes.find({"code_shop" : idShop}).sort('price').exec(function(err, docs) {
+            Shoes.find(query).sort('price').exec(function(err, docs) {
                 allshoes = docs;
                 res.render('shoes',{shop: shop,allshoes:allshoes});
             });
         }else{
-            Shoes.find({"code_shop" : idShop}).sort('-price').exec(function(err, docs) {
+            Shoes.find(query).sort('-price').exec(function(err, docs) {
                 allshoes = docs;
                 res.render('shoes',{shop: shop,allshoes:allshoes});
             });
