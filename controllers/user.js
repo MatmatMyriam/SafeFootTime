8716d204819c22300441ab81b0328b810239d2b8
thibaudG <thibaud.grepin@gmail.com>
2017-11-20 02:25:09 +0100

    up

--- controllers/user.js
+++ controllers/user.js
@@ -209,8 +209,6 @@ function UpdateDiscount(userid, price){
   });
 }
 
-module.exports.UpdateDiscount=UpdateDiscount;
-
 /*
  * async function who use promesses
  * return the total discount price, this discount is decreased from the user discount
@@ -236,7 +234,6 @@ function UseDiscount(userid, price){
     });
   });
 }
-
 /*
 exports.GetTest = (req, res, next) =>{
   UseDiscount(req.user.id, req.params.price).then(function (discount){
